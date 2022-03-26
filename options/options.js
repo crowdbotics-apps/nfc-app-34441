1fc82feb7139f41cca2c5a2f6982f3dd53125685
Crowdbotics <team@crowdbotics.com>
2022-03-26 01:52:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nfc_app_34441",
+  url: "https://nfc_app_34441.botics.co",
+  api: "https://nfc_app_34441.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
